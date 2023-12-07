1eff5abb09e761ace2909b0c83d47e67b2a0c943
CrispyJLoHalo <jonahlottner02@gmail.com>
2023-12-07 14:02:10 +0100

    Made funcs more concise

--- code_data_gen/data_gen_cows/data_gen_cows_client.py
+++ code_data_gen/data_gen_cows/data_gen_cows_client.py
@@ -21,8 +21,17 @@ def is_in_pasture(point, fence):
         else:
             return False
 
-def pub_gps_data(cow, point):
-    msg = "Cow %s \nLon: %s \nLat: %s" % (cow.id, point.y, point.x)
+def get_longest_movement(list_cows):
+    all_coords = []
+    for cow in list_cows:
+        all_coords.append(cow.movement_points)
+    return len(max(all_coords, key=len))
+
+def pub_gps_data(cow, point, check):
+    if check:
+        msg = "Cow %s \nLon: %s \nLat: %s" % (cow.id, point.y, point.x)
+    else:
+        msg = "No GPS Data for Cow " + str(cow.id)
     mqtt_client_gps.publish(mqtt_pub_topic_gps,
                             payload=msg,
                             qos=2,
@@ -30,60 +39,50 @@ def pub_gps_data(cow, point):
 
 def pub_alarm(cow, point, fence):
     if is_in_pasture(point, fence):
-        msg = "Cow %s is in the pasture" % cow.id
+        msg_txt = "Cow %s is in the pasture" % cow.id
+        msg_bool = True
+    elif is_in_pasture(point, fence) is False:
+        msg_txt = "Cow %s is NOT in the pasture!" % cow.id
+        msg_bool = False
     else:
-        msg = "Cow %s is NOT in the pasture!" % cow.id
-    mqtt_client_alarm.publish(mqtt_pub_topic_alarm,
-                              payload=msg,
+        msg_txt = "No Alarm Data for Cow " + str(cow.id)
+        msg_bool = None
+    mqtt_client_alarm_txt.publish(mqtt_pub_topic_alarm_txt,
+                              payload=msg_txt,
                               qos=2,
                               retain=False)
-
-def pub_no_data_alarm(cow):
-    msg = "No Alarm Data for Cow " + str(cow.id)
-    mqtt_client_alarm.publish(mqtt_pub_topic_alarm,
-                              payload=msg,
+    mqtt_client_alarm_bool.publish(mqtt_pub_topic_alarm_bool,
+                              payload=msg_bool,
                               qos=2,
                               retain=False)
     
-def pub_no_data_gps(cow):
-    msg = "No GPS Data for Cow " + str(cow.id)
-    mqtt_client_alarm.publish(mqtt_pub_topic_gps,
-                              payload=msg,
-                              qos=2,
-                              retain=False)
-
 def pub_all(list_cows, fence):
     while True:
         for i in range(get_longest_movement(list_cows)):
             for cow in list_cows:
-                if i < len(cow.movement_points):
+                check = i<len(cow.movement_points)
+                if check:
                     pub_alarm(cow, cow.movement_points[i], fence)
+                    pub_gps_data(cow, cow.movement_points[i], check)
                 else: 
-                    pub_no_data_alarm(cow)
-            for cow in list_cows:
-                if i < len(cow.movement_points):
-                    pub_gps_data(cow, cow.movement_points[i])
-                else:
-                    pub_no_data_gps(cow)
+                    pub_alarm(cow, cow.movement_points[i], fence)
+                    pub_gps_data(cow, cow.movement_points[i], check)
             time.sleep(5)
-
-def get_longest_movement(list_cows):
-    all_coords = []
-    for cow in list_cows:
-        all_coords.append(cow.movement_points)
-    return len(max(all_coords, key=len))
 
 #Definition MQTT Information
 mqtt_broker_adr = "wi-vm162-01.rz.fh-ingolstadt.de"
 mqtt_broker_port = 1870
 mqtt_pub_topic_gps = "group4/livestock/gps_data"
-mqtt_pub_topic_alarm = "group4/livestock/alarms"
+mqtt_pub_topic_alarm_txt = "group4/livestock/alarms/txt"
+mqtt_pub_topic_alarm_bool = "group4/livestock/alarms/bool"
 #Clients Erstellen
 mqtt_client_gps = mqtt.Client()
-mqtt_client_alarm = mqtt.Client()
+mqtt_client_alarm_txt = mqtt.Client()
+mqtt_client_alarm_bool = mqtt.Client()
 #Clients Verbinden
 mqtt_client_gps.connect(host=mqtt_broker_adr, port=mqtt_broker_port)
-mqtt_client_alarm.connect(host=mqtt_broker_adr, port=mqtt_broker_port)
+mqtt_client_alarm_txt.connect(host=mqtt_broker_adr, port=mqtt_broker_port)
+mqtt_client_alarm_bool.connect(host=mqtt_broker_adr, port=mqtt_broker_port)
 
 #Lesen der Grenzen des virtuellen Zauns
 with open("./code_data_gen/data_gen_cows/geodata/boundaries_pasture.json", "r") as read_file:
